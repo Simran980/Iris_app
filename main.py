917e4fbface1572a2b3ab102ed0a22f09c09de8c
Simran980 <simran.cse18@satyug.edu.in>
2021-10-10 03:00:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from prep_data import df, X_train, y_train, X, y
+from display_func import pred_page, graph_p
+
+# Add sidbar title
+st.sidebar.title("Menu")
+
+nav = st.sidebar.radio('Navigator', ('Home', 'Working', 'Prediction', 'Graph', 'Contact Us'))
+
+if nav == 'Prediction':
+	st.title("Iris Flower Species Prediction App") 
+	pred_page(df, X_train, y_train)
+
+elif nav == 'Home':
+    st.image('images\welcome.jpg' , width=500)
+    st.title('Welcome to Iris Flower Species Prediction App')
+    st.markdown('### This website predicts the species of iris flower with different Machine learning classificaion model on the basis of given data')
+    st.subheader('Data Used')
+    d_check = st.checkbox('Show data used')
+    if d_check:
+	    st.dataframe(df, width=1000, height=300)
+
+elif nav == 'Working':
+	st.header('Working of This Website')
+	st.subheader('Types of Iris Flower Species')
+	st.image('images\iris_types.png', width = 500)
+	st.subheader('How Species are predicted')
+	st.image('images\iris_c.png')
+
+elif nav == 'Graph':
+	st.title('Graph for prediction model')
+	graph_p(X, y)
+
+else:
+	st.balloons()
+	st.header('Contact Us')
+	st.markdown('''### Name:
+	Simran''')
+	st.markdown('''### GitHub: [Simran](https://github.com/Simran980?tab=repositories)''')
---
+++ prep_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Loading the dataset.
+df = pd.read_csv("iris-species.csv")
+
+# Adding a column in the Iris DataFrame to resemble the non-numeric 'Species' column as numeric using the 'map()' function.
+# Creating the numeric target column 'Label' to 'iris_df' using the 'map()' function.
+df['Label'] = df['Species'].map({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor':2})
+
+# Creating a model for Support Vector classification to classify the flower types into labels '0', '1', and '2'.
+
+# Creating features and target DataFrames.
+X = df[['SepalLengthCm','SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = df['Label']
+
+# Splitting the data into training and testing sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
